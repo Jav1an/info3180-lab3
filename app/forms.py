79c0097cc8e88e80e7d97cfb62c2a9cfe4dbe9da
Jav1an <javiansaunders123@gmail.com>
2025-02-27 08:48:34 -0500

    Added contach.html and css.

---
+++ app/forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField
+from wtforms.validators import DataRequired, Email, length, Regexp
+
+class ContactForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired(), length(max=150), Regexp(r'^[A-Za-z\s\-]+$')])
+    email = StringField('E-mail', validators=[DataRequired(), Email(), length(max=150)])
+    subject = StringField('Subject', validators=[DataRequired(), length(max=200)])
+    message = TextAreaField('Message', validators=[DataRequired()])
--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
-from app import app
+from app import app, config
 from flask import render_template, request, redirect, url_for, flash
-
+from .forms import ContactForm
+from app import mail
+from flask_mail import Message
 
 ###
 # Routing for your application.
@@ -17,7 +19,28 @@ def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
+@app.route('/contact/', methods=['GET', 'POST'])
+def contact():
+    """Render the website's contact page."""
+    form = ContactForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        subject = form.subject.data
+        message = form.message.data
+        try:
+            send_email(name, email, subject, message)
+            return redirect(url_for('home'))
+        except Exception as e:
+            flash(f"Failed to send email. Error: {str(e)}", "danger")
+    flash_errors(form)
+    return render_template('contact.html', form=form)
 
+def send_email(name, email, subject, message):
+    msg = Message(subject, sender=(name, email), recipients=["maryjane@example.com"])
+    msg.body = message
+    mail.send(msg)
+    flash('Email Successfully Sent With credential:- Name: {} Email: {} Subject: {} Message: {}'.format(name, email, subject, message), 'success')
 ###
 # The functions below should be applicable to all Flask apps.
 ###
