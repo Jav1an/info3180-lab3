d3f46e7cea55e28b69ce0f159fb3d210eabf30f2
Jav1an <javiansaunders123@gmail.com>
2025-02-27 08:55:09 -0500

    Added Error message to name validation.

--- app/forms.py
+++ app/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, TextAreaField
 from wtforms.validators import DataRequired, Email, length, Regexp
 
 class ContactForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired(), length(max=150), Regexp(r'^[A-Za-z\s\-]+$')])
+    name = StringField('Name', validators=[DataRequired(), length(max=150), Regexp(r'^[A-Za-z\s\-]+$', message="Name must contain only letters")])
     email = StringField('E-mail', validators=[DataRequired(), Email(), length(max=150)])
     subject = StringField('Subject', validators=[DataRequired(), length(max=200)])
     message = TextAreaField('Message', validators=[DataRequired()])
